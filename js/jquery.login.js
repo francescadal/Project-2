5ec8feb28d2f7be701c14e36e76fdc596e30a359
francescadal <francescadalembert@icloud.com>
2015-11-12 14:36:40 -0500

    Editing the html Added login page. trying to fix little things

--- js/jquery.login.js
+++ js/jquery.login.js
@@ -1,9 +1,26 @@
-$(document).ready(function(){
-  $('#login-trigger').click(function(){
-    $(this).next('#login-content').slideToggle();
-    $(this).toggleClass('active');
+$(function() {
+  // constants
+  var SHOW_CLASS = 'show',
+      HIDE_CLASS = 'hide',
+      ACTIVE_CLASS = 'active';
 
-    if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
-      else $(this).find('span').html('&#x25BC;')
-    })
+  $( '.tabs' ).on( 'click', 'li a', function(e){
+    e.preventDefault();
+    var $tab = $( this ),
+         href = $tab.attr( 'href' );
+
+     $( '.active' ).removeClass( ACTIVE_CLASS );
+     $tab.addClass( ACTIVE_CLASS );
+
+     $( '.show' )
+        .removeClass( SHOW_CLASS )
+        .addClass( HIDE_CLASS )
+        .hide();
+
+      $(href)
+        .removeClass( HIDE_CLASS )
+        .addClass( SHOW_CLASS )
+        .hide()
+        .fadeIn( 550 );
+  });
 });
